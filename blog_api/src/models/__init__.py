12b7f7d19799414ff5aa67defcaa31fccd246d2f
Rinku92 <rp744@njit.edu>
2019-12-18 14:06:04 -0500

    Fix: Updated files.

--- blog_api/src/models/__init__.py
+++
@@ -1,9 +0,0 @@
-#src/models/__init__.py
-
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-#######
-# existing code remains #
-db = SQLAlchemy()
-#######
-bcrypt = Bcrypt()
