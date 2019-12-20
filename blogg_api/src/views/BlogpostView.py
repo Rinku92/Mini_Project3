c643a1e76aa82b30981989c0c13c66f4f8fb30c5
Rinku92 <rp744@njit.edu>
2019-12-20 09:12:45 -0500

    Feature: added code in blogpostview

--- blogg_api/src/views/BlogpostView.py
+++ blogg_api/src/views/BlogpostView.py
@@ -6,22 +6,21 @@ from ..models.BlogpostModel import BlogpostModel, BlogpostSchema
 blogpost_api = Blueprint('blogpost_api', __name__)
 blogpost_schema = BlogpostSchema()
 
-
 @blogpost_api.route('/', methods=['POST'])
 @Auth.auth_required
 def create():
-    """
-    Create Blogpost Function
-    """
-    req_data = request.get_json()
-    req_data['owner_id'] = g.user.get('id')
-    data, error = blogpost_schema.load(req_data)
-    if error:
-        return custom_response(error, 400)
-    post = BlogpostModel(data)
-    post.save()
-    data = blogpost_schema.dump(post).data
-    return custom_response(data, 201)
+  """
+  Create Blogpost Function
+  """
+  req_data = request.get_json()
+  req_data['owner_id'] = g.user.get('id')
+  data, error = blogpost_schema.load(req_data)
+  if error:
+    return custom_response(error, 400)
+  post = BlogpostModel(data)
+  post.save()
+  data = blogpost_schema.dump(post).data
+  return custom_response(data, 201)
 
 
 @blogpost_api.route('/', methods=['GET'])
@@ -87,11 +86,11 @@ def delete(blogpost_id):
 
 
 def custom_response(res, status_code):
-    """
-    Custom Response Function
-    """
-    return Response(
-        mimetype="application/json",
-        response=json.dumps(res),
-        status=status_code
-    )
+  """
+  Custom Response Function
+  """
+  return Response(
+    mimetype="application/json",
+    response=json.dumps(res),
+    status=status_code
+  )
